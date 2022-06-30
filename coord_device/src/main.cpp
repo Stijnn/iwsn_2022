30a3d86091fb4ce67bb29e39b49332c0cadfcc3d
Stijn Verhelpen <stijn.verhelpen@live.nl>
2022-06-30 12:06:49 +0200

    Additions

--- coord_device/src/main.cpp
+++ coord_device/src/main.cpp
@@ -1,11 +1,15 @@
 #include <Arduino.h>
+#include <pins_arduino.h>
 #include <SPI.h>
 #include <Ethernet.h>
 #include <PubSubClient.h>
-
+#include <SoftwareSerial.h>
+#include <XBee.h>
 
 EthernetClient eth;
 PubSubClient mqtt;
+XBee xbee = XBee();
+ZBRxResponse rx = ZBRxResponse();
 
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 IPAddress ip(192, 168, 1, 50);
@@ -15,12 +19,10 @@ IPAddress subnet(255, 255, 255, 0);
 
 void connect_mqtt()
 {
-  Serial.println("Attempting Connect");
-
   if (mqtt.connected())
     return;
 
-  if (mqtt.connect("IWSN_COORDINATOR"))
+  if (mqtt.connect("IWSN_COORDINATOR", "coordinator", "secretpassword"))
   {
     mqtt.publish("AUTH", "CONNECTED");
   }
@@ -33,22 +35,44 @@ void connect_mqtt()
 
 
 void setup() {
-  Ethernet.begin(mac, ip, dns, gateway, subnet);
+    Ethernet.begin(mac, ip, dns, gateway, subnet);
 
-  Serial.begin(9600);
+    Serial.begin(9600);
+    xbee.begin(Serial);
 
-  mqtt.setClient(eth);
-  mqtt.setBufferSize(256);
-  mqtt.setServer(IPAddress(192, 168, 1, 9), 9001);
+    mqtt.setClient(eth);
+    mqtt.setBufferSize(256);
+    mqtt.setServer(IPAddress(192, 168, 1, 19), 9001);
 
-  Serial.println(eth.status());
-  Serial.print("Coordinator Connected from IP: ");
-  Serial.println(Ethernet.localIP());
+    Serial.println(eth.status());
+    Serial.print("Coordinator Connected from IP: ");
+    Serial.println(Ethernet.localIP());
 
-  connect_mqtt();
+    connect_mqtt();
 }
 
 
 void loop() {
-  delay(1000);
+    if (!mqtt.connected())
+        connect_mqtt();
+
+    if (xbee.readPacket(500))
+    {
+        if (xbee.getResponse().isAvailable())
+        {
+            mqtt.publish("DATA", "New Packet");
+            if (xbee.getResponse().getApiId() == ZB_RX_RESPONSE)
+            {
+                xbee.getResponse().getZBRxResponse(rx);
+                mqtt.publish("DATA", "Packet parsed");
+
+                char payload[rx.getDataLength()];
+                uint8_t* xbee_buffer = rx.getData();
+                memcpy(payload, xbee_buffer, rx.getDataLength());
+                mqtt.publish("DATA", payload, rx.getDataLength());
+            }
+        }
+    }
+
+    delay(1000);
 }
--- end_device/src/main.cpp
+++ end_device/src/main.cpp
@@ -1,9 +1,64 @@
-#include <Arduino.h>
+#include <XBee.h>
+#include <DHT.h>
+#include <DHT_U.h>
+
+XBee xbee = XBee();
+
+DHT_Unified dht(PIN_A0, DHT11);
+
+XBeeAddress64 coordinator_address = XBeeAddress64(0x0013A200, 0x419313D2);
+ZBTxStatusResponse tx_status = ZBTxStatusResponse();
+
+typedef union
+{
+	float value;
+	unsigned char buffer[4];
+} float_sensor_data;
+
 
 void setup() {
-
+	Serial.begin(9600);
+	xbee.begin(Serial);
+	dht.begin();
 }
 
 void loop() {
+	sensors_event_t temp_event, humidity_event;
+	if (dht.temperature().getEvent(&temp_event))
+	{
+		if (!isnan(temp_event.temperature))
+		{
+			Serial.println(temp_event.temperature);
+		}
+	}
 
+	if (dht.humidity().getEvent(&humidity_event))
+	{
+		if (!isnan(humidity_event.relative_humidity))
+		{
+			Serial.println(humidity_event.relative_humidity);
+		}
+	}
+  
+	char temperature_str[8];
+	strcpy(temperature_str, "");
+	dtostrf(temp_event.temperature, 3, 2, &temperature_str[strlen(temperature_str)]);
+
+	char humidity_str[8];
+	strcpy(humidity_str, "");
+	dtostrf(humidity_event.relative_humidity, 3, 2, &humidity_str[strlen(humidity_str)]);
+
+	char msg[17];
+	sprintf(msg,"%s %s", temperature_str, humidity_str);
+	
+	ZBTxRequest dht_sensor_data = ZBTxRequest(coordinator_address, (uint8_t*)msg, 17);
+	xbee.send(dht_sensor_data);
+
+	if (xbee.readPacket(500)) {         	
+		if (xbee.getResponse().getApiId() == ZB_TX_STATUS_RESPONSE) {
+			xbee.getResponse().getZBTxStatusResponse(tx_status);
+		}
+	}
+
+	delay(1000);
 }
